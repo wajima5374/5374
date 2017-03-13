564369fafbb67a2dbac47d779a260130b6dfab9a
sss3s <aacr0825p@yahoo.co.jp>
2017-03-14 07:42:36 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -534,9 +534,9 @@ $(function() {
     var styleHTML = "";
     var accordionHTML = "";
 
-    // 20170313 sss3s追加 どの地域を選択したかわかりやすく
-    styleHTML += '#no-accordion-group{background-color:#f5f5f5; color:#005098; padding:30px 0px; font-size: 18pt;}';
-    accordionHTML += '<div id="no-accordion-group"  align="center">' + areaModels[row_index].label + 'の収集日</div>';
+    // 20170314 sss3s追加 どの地域を選択したかわかりやすく
+    styleHTML += '#no-accordion{background-color:#f5f5f5; color:#005098; padding:25px 0px; font-size: 18pt;}';
+    accordionHTML += '<div id="no-accordion"  align="center">' + areaModels[row_index].label + 'の収集日</div>';
 
     //アコーディオンの分類から対応の計算を行います。
     for (var i in areaModel.trash) {
